ec30461d636dec64a4cc25ab16560730b7c20e14
cristhian fajardo <cristhian.fajardo@estudiants.urv.cat>
2022-01-17 15:08:25 +0100

    Generador laberinto aleatorio

--- src/aplicacio/Main.java
+++ src/aplicacio/Main.java
@@ -1,4 +1,5 @@
 package aplicacio;
+
 /**
 *
 * @author Cristhian y Arnau
@@ -10,30 +11,30 @@ import dades.SolucionadorLaberinto;
 public class Main {
 
 	public static void main(String[] args) {
-        String[][] laberint = {
-        		{"NA", "NA", "-1", "/2", "+1", "NA", "NA"},
-                {"-7", "NA", "*3", "NA", "+2", "+1", "-1"},
-                {"+4", "*2", "-8", "+3", "+7", "-1", "+4"},
-                {"+9", "-2", "+5", "-1", "+9", "+8", "-2"},
-                {"/1", "*7", "NA", "NA", "+4", "NA", "NA"}};
-        
-        String[][] laberint2 = {
-        		{"NA", "NA", "-1", "/2", "+1", "NA", "NA"},
-                {"-7", "NA", "*3", "NA", "+2", "+1", "-1"},
-                {"+4", "*2", "-8", "+3", "+7", "-1", "+4"},
-                {"+9", "-2", "+5", "-1", "+9", "+8", "-2"},
-                {"/1", "*7", "NA", "NA", "+4", "NA", "NA"}};
-        
-        String[][] laberint3 = {
-        		{"NA", "NA", "-1", "/2", "+1", "NA", "NA"},
-                {"*3", "NA", "*3", "NA", "+2", "+1", "-1"},
-                {"+53", "-5", "-8", "+3", "+7", "-1", "+4"},
-                {"+129", "-2", "+5", "-1", "+9", "+8", "-2"},
-                {"/1", "-7", "NA", "NA", "+4", "NA", "NA"}};
-            Laberinto lab = new Laberinto(laberint3, 6, 4,new Posicion(3,0), new Posicion(1,6));
-            SolucionadorLaberinto solv = new SolucionadorLaberinto(lab);
-            System.out.print("COMENZAMOS\n");
-            System.out.println("Ho he resolt greedy: "+ solv.solveDfs() );
-        }
-        
-    }
+		String[][] laberint = { { "NA", "NA", "-1", "/2", "+1", "NA", "NA" },
+				{ "-7", "NA", "*3", "NA", "+2", "+1", "-1" }, { "+4", "*2", "-8", "+3", "+7", "-1", "+4" },
+				{ "+9", "-2", "+5", "-1", "+9", "+8", "-2" }, { "/1", "*7", "NA", "NA", "+4", "NA", "NA" } };
+
+		String[][] laberint2 = { { "NA", "NA", "-1", "/2", "+1", "NA", "NA" },
+				{ "-7", "NA", "*3", "NA", "+2", "+1", "-1" }, { "+4", "*2", "-8", "+3", "+7", "-1", "+4" },
+				{ "+9", "-2", "+5", "-1", "+9", "+8", "-2" }, { "/1", "*7", "NA", "NA", "+4", "NA", "NA" } };
+
+		String[][] laberint3 = { { "NA", "NA", "-1", "/2", "+1", "NA", "NA" },
+				{ "*3", "NA", "*3", "NA", "+2", "+1", "-1" }, { "+53", "-5", "-8", "+3", "+7", "-1", "+4" },
+				{ "+129", "-2", "+5", "-1", "+9", "+8", "-2" }, { "/1", "-7", "NA", "NA", "+4", "NA", "NA" } };
+		Laberinto lab = new Laberinto(laberint3, 6, 4, new Posicion(3, 0), new Posicion(1, 6));
+
+		System.out.print("COMENZAMOS\n");
+
+		Laberinto lab2 = new Laberinto(7, 7);
+		for (int i = 0; i <= lab2.getAlto(); i++) {
+			for (int j = 0; j <= lab2.getAncho(); j++) {
+				System.out.print(lab2.getLaberinto()[i][j] + " ");
+			}
+			System.out.println();
+		}
+		SolucionadorLaberinto solv = new SolucionadorLaberinto(lab2);
+		System.out.println("Ho he resolt greedy: " + solv.solveDfs());
+	}
+
+}
--- src/dades/Laberinto.java
+++ src/dades/Laberinto.java
@@ -1,132 +1,194 @@
 package dades;
+
+import java.util.Iterator;
+
 /**
-*
-* @author Cristhian y Arnau
-*/
+ *
+ * @author Cristhian y Arnau
+ */
+
 public class Laberinto {
-    private String[][] laberinto;
-    private int ancho;
-    private int alto;
-    private Posicion entrada;	//Posicion de entrada del laberinto
-    private Posicion salida;	//Posicion de salida del laberinto
+	private String[][] laberinto;
+	private int ancho; // Columnas
+	private int alto; // Filas
+	private Posicion entrada; // Posicion de entrada del laberinto
+	private Posicion salida; // Posicion de salida del laberinto
 
-    public Laberinto(String[][] lab, int ancho, int alto, Posicion entrada, Posicion salida) {
-        this.laberinto = lab;
-        this.ancho = ancho; //maxima posicio habitable 
-        this.alto = alto;	//maxima posicion habitable
-        this.entrada = entrada;
-        this.salida = salida;
-    }
+	//Por falta de tiempo he implementado un algoritmo algo mas simple pero en mayor medida funcional
+	public Laberinto(int ancho, int alto) {
+		this.ancho = ancho;
+		this.alto = alto;
+		laberinto = new String[alto+1][ancho+1];
+		for (int i = 0; i <= alto; i++) {
+			for (int j = 0; j <= ancho; j++) {
+				int aleatorio = (int) (Math.random() * 5) + 1;
+				if (aleatorio == 1) {
+					laberinto[i][j] = "NA";
+				} else
+					laberinto[i][j] = generarPosRandom();
+			}
 
-    public String getAt(Posicion p){
-        return laberinto[p.getX()][p.getY()];
-    }
-    
-    
-    public Posicion getEntrada() {
-            return entrada;
-    }
+		}
+		boolean inicial = false;
+		for (int i = 0; i <= alto; i++) {
+			for (int j = 0; j <= ancho; j++) {
+				if(laberinto[i][j] != "NA" && !inicial && laberinto[i][j].charAt(0) == '+') {
+					entrada = new Posicion(i, j);
+					inicial = true;
+				}else
+					salida = new Posicion(i, j);
+			}
 
-    public void setEntrada(Posicion entrada) {
-            this.entrada = entrada;
-    }
+		}
+		System.out.print(entrada+" "+salida+"\n");
+	}
 
-    public Posicion getSalida() {
-            return salida;
-    }
+	//Metodo para generar
+	private String generarPosRandom() {
+		int aleatorio = (int) (Math.random() * 4)+1;	//Random para escoger que operacion a realizar
+		String caracterOp = "";
+		int numRandom =(int)(Math.random()*15)+1;	//random para el numero en la posicion
+		switch (aleatorio) {
+		case 0:
+			caracterOp = "+"+String.valueOf(numRandom);
+			System.out.println("He escogido + " + aleatorio);
+			break;
+		case 1:
+			caracterOp = "-"+String.valueOf(numRandom);
+			System.out.println("He escogido - " + aleatorio);
+			break;
+		case 2:
+			caracterOp = "*"+String.valueOf(numRandom);
+			System.out.println("He escogido x "+ aleatorio);
+			break;
+		case 3:
+			caracterOp = "+"+String.valueOf(numRandom);
+			System.out.println("He escogido + " + aleatorio);
+			break;
+		default:
+			caracterOp = "/"+String.valueOf(numRandom);
+			System.out.println("He escogido / "+ aleatorio);
+			break;
+		}
+		return caracterOp;
+	}
 
-    public void setSalida(Posicion salida) {
-            this.salida = salida;
-    }
+	public Laberinto(String[][] lab, int ancho, int alto, Posicion entrada, Posicion salida) {
+		this.laberinto = lab;
+		this.ancho = ancho; // maxima posicio habitable
+		this.alto = alto; // maxima posicion habitable
+		this.entrada = entrada;
+		this.salida = salida;
+	}
 
-    public String[][] getLaberinto() {
-            return laberinto;
-    }
+	public String getAt(Posicion p) {
+		return laberinto[p.getX()][p.getY()];
+	}
 
-    public void setLaberinto(String[][] laberinto) {
-            this.laberinto = laberinto;
-    }
+	public Posicion getEntrada() {
+		return entrada;
+	}
 
-    public int getAncho() {
-            return ancho;
-    }
+	public void setEntrada(Posicion entrada) {
+		this.entrada = entrada;
+	}
 
-    public void setAncho(int ancho) {
-            this.ancho = ancho;
-    }
+	public Posicion getSalida() {
+		return salida;
+	}
 
-    public int getAlto() {
-            return alto;
-    }
+	public void setSalida(Posicion salida) {
+		this.salida = salida;
+	}
 
-    public void setAlto(int alto) {
-            this.alto = alto;
-    }
+	public String[][] getLaberinto() {
+		return laberinto;
+	}
 
-    //Mira si el laberinto esta solucionado
-    public static boolean IsSolved(Posicion actual, Laberinto lab) {
-    	return actual.getX() == lab.getSalida().getX() && actual.getY() ==  lab.getSalida().getY();	
-    }
-    
-    //Devuelve los hijos del nodo actual para despues decidir a cual nos movemos.
-    public static Posicion[] GetSuccessors(Posicion actual, Laberinto lab){
-        boolean [] valids = {false, false, false, false}; //inf, sup, izq, der
-        int conta = 0;
-        if(actual.getX() != lab.getAlto() && ! lab.getAt(actual.getInferior()).equals("NA")) {
-            valids[0] = true;
-            conta ++;
-        }
-        if(actual.getX() != 0  && ! lab.getAt(actual.getSuperior()).equals("NA")){
-            valids[1] = true;
-            conta ++;
-        }
-        if(actual.getY() != 0  && ! lab.getAt(actual.getIzquierda()).equals("NA")){
-            valids[2] = true;
-            conta ++;
-        }
-        if(actual.getY() != lab.getAncho()  && ! lab.getAt(actual.getDerecha()).equals("NA")){
-            valids[3] = true;
-            conta ++;
-        }
-        Posicion [] retorn = new Posicion[conta];
-        conta = 0;
-        for(int i = 0; i < 4; i++){
-            if(valids[i]){
-                if(i == 0){
-                    retorn[conta] = actual.getInferior();
-                    retorn[conta].setScore(actual.combinaScore(lab.getAt(actual.getInferior())));
-                    conta ++;
-                } else if(i == 1){
-                    retorn[conta] = actual.getSuperior();
-                    retorn[conta].setScore(actual.combinaScore(lab.getAt(actual.getSuperior())));
-                    conta ++;
-                } else if(i == 2){
-                    retorn[conta] = actual.getIzquierda();
-                    retorn[conta].setScore(actual.combinaScore(lab.getAt(actual.getIzquierda())));
-                    conta ++;
-                } else if(i == 3){
-                    retorn[conta] = actual.getDerecha();
-                    retorn[conta].setScore(actual.combinaScore(lab.getAt(actual.getDerecha())));
-                    conta ++;
-                }
-            }
-        }
-        
-        return sort(retorn, conta);
-    }
-    
-    //Buble sort para ordenar la pila
-    private static Posicion [] sort(Posicion [] p, int mida){
-        for(int i = 0; i < mida; i++){
-            for(int j = i; j < mida; j++){
-                if (p[j].getScore() < p[i].getScore()){
-                    Posicion aux = p[j];
-                    p[j] = p[i];
-                    p[i] = aux;
-                }
-            }
-        }
-        return p;
-    }
+	public void setLaberinto(String[][] laberinto) {
+		this.laberinto = laberinto;
+	}
 
+	public int getAncho() {
+		return ancho;
+	}
+
+	public void setAncho(int ancho) {
+		this.ancho = ancho;
+	}
+
+	public int getAlto() {
+		return alto;
+	}
+
+	public void setAlto(int alto) {
+		this.alto = alto;
+	}
+
+	// Mira si el laberinto esta solucionado
+	public static boolean IsSolved(Posicion actual, Laberinto lab) {
+		return actual.getX() == lab.getSalida().getX() && actual.getY() == lab.getSalida().getY();
+	}
+
+	// Devuelve los hijos del nodo actual para despues decidir a cual nos movemos.
+	public static Posicion[] GetSuccessors(Posicion actual, Laberinto lab) {
+		boolean[] valids = { false, false, false, false }; // inf, sup, izq, der
+		int conta = 0;
+		if (actual.getX() != lab.getAlto() && !lab.getAt(actual.getInferior()).equals("NA")) {
+			valids[0] = true;
+			conta++;
+		}
+		if (actual.getX() != 0 && !lab.getAt(actual.getSuperior()).equals("NA")) {
+			valids[1] = true;
+			conta++;
+		}
+		if (actual.getY() != 0 && !lab.getAt(actual.getIzquierda()).equals("NA")) {
+			valids[2] = true;
+			conta++;
+		}
+		if (actual.getY() != lab.getAncho() && !lab.getAt(actual.getDerecha()).equals("NA")) {
+			valids[3] = true;
+			conta++;
+		}
+		Posicion[] retorn = new Posicion[conta];
+		conta = 0;
+		for (int i = 0; i < 4; i++) {
+			if (valids[i]) {
+				if (i == 0) {
+					retorn[conta] = actual.getInferior();
+					retorn[conta].setScore(actual.combinaScore(lab.getAt(actual.getInferior())));
+					conta++;
+				} else if (i == 1) {
+					retorn[conta] = actual.getSuperior();
+					retorn[conta].setScore(actual.combinaScore(lab.getAt(actual.getSuperior())));
+					conta++;
+				} else if (i == 2) {
+					retorn[conta] = actual.getIzquierda();
+					retorn[conta].setScore(actual.combinaScore(lab.getAt(actual.getIzquierda())));
+					conta++;
+				} else if (i == 3) {
+					retorn[conta] = actual.getDerecha();
+					retorn[conta].setScore(actual.combinaScore(lab.getAt(actual.getDerecha())));
+					conta++;
+				}
+			}
+		}
+
+		return sort(retorn, conta);
+	}
+
+	// Buble sort para ordenar la pila
+	private static Posicion[] sort(Posicion[] p, int mida) {
+		for (int i = 0; i < mida; i++) {
+			for (int j = i; j < mida; j++) {
+				if (p[j].getScore() < p[i].getScore()) {
+					Posicion aux = p[j];
+					p[j] = p[i];
+					p[i] = aux;
+				}
+			}
+		}
+		return p;
+	}
+	
 }
